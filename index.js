71fe618448dc0460891582283ad7184a276ca482
Rc Rao <93970364+chamanyadav123@users.noreply.github.com>
2024-08-22 22:55:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // this is index.js
+import dotenv from "dotenv";
+import connectDB from "./src/db/index.js";
+import { app } from "./src/app.js";
+
+dotenv.config({
+    path: '/.env'
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000,
+        () => {
+            console.log(`Server is running at port ${process.env.PORT}`)
+        }
+    )
+})
+.catch((err) => {
+    console.log(`Error Database is not connecting`, err)
+})
+
