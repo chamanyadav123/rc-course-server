f28bcae1b8c59b1358fd9231ef068661f4bf4248
Rc Rao <93970364+chamanyadav123@users.noreply.github.com>
2024-08-22 23:38:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
-const app = express()
-
-
-const cors = require('cors');
-const express = require('express');
 const app = express();
 
 // Apply CORS middleware
@@ -17,22 +12,20 @@ app.use(cors({
     credentials: true, // If cookies or other credentials are involved
 }));
 
-// Your routes and other middleware
+// Middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+// Routes
+import adminRouter from "./routes/admin.routes.js";
+app.use("/api/admin", adminRouter);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded())
-app.use(express.static("public"))
-app.use(cookieParser())
+import userRouter from "./routes/user.routes.js";
+app.use("/api/user", userRouter);
 
+// Handle OPTIONS requests
+app.options('*', cors());
 
-
-import adminRouter from "./routes/admin.routes.js"
-app.use("/api/admin", adminRouter)
-
-
-
-import userRouter from "./routes/user.routes.js"
-app.use("/api/user", userRouter)
-
-export {app}
+export { app };
